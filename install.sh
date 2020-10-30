e0fa54964726487fdb11d7d7383f84eaa13019e2
cys8607 <50197618+cys8607@users.noreply.github.com>
2020-10-30 16:26:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,4 @@ pip install suricata-update
 docker pull sebp/elk
 docker pull jasonish/evebox:latest
 
+docker run -it -m 4G -v /var/log/suricata:/mnt -p 5601:5601 -p 9200:9200 -p 5044:5044 sebp/elk
