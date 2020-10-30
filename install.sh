c0503c1902c5909387da7d5585b619928a696d51
cys8607 <50197618+cys8607@users.noreply.github.com>
2020-10-30 16:27:47 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,7 @@ docker pull sebp/elk
 docker pull jasonish/evebox:latest
 
 docker run -it -m 4G -v /var/log/suricata:/mnt -p 5601:5601 -p 9200:9200 -p 5044:5044 sebp/elk
+
+
+https://docs.microsoft.com/ko-kr/azure/network-watcher/network-watcher-intrusion-detection-open-source-tools
+https://www.cnblogs.com/zafu/p/12141909.html
